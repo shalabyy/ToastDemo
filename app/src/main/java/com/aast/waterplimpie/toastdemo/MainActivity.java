1ed0928460c7ad0c25ccfa2de6c913070ce01dce
WaterPlimpie <randa.hamdy@hotmail.com>
2016-10-21 16:13:09 +0200

    Second Trial

--- app/src/main/java/com/aast/waterplimpie/toastdemo/MainActivity.java
+++ app/src/main/java/com/aast/waterplimpie/toastdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText tf = (EditText) findViewById(R.id.name);
         Button btn = (Button) findViewById(R.id.button);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
